e8784a8dfb471653f971489e377e77863b772cb4
YJ Soon <yjsoon@tinkertanker.com>
2024-05-20 16:43:44 +0800

    Added stack navigation on home screen

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* // Help me style the Hello World text to be large and red and centered */}
-      <Text style={{ fontSize: 50, color: "red", textAlign: "center" }}>
-        Hello World
-      </Text>
-      <Text>Does this work???</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
